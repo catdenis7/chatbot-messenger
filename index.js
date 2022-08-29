d297d40727676774242dfdfa8795114bc1f40b3b
catdenis7 <catdenisgomez7@gmail.com>
2022-08-28 22:53:00 -0400

    estructura de controladores creada

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import chatbot from "./controller/chatbot";
+var chatbot = require("./controller/chatbot");
 // Importar las dependencias para configurar el servidor
 var express = require("express");
 var request = require("request");
@@ -16,76 +16,9 @@ app.get("/", (req, res)=> chatbot.index(req, res));
 // Facebook Webhook
 
 // Usados para la verificacion
-app.get("/webhook", function (req, res) {
-    // Verificar la coincidendia del token
-    if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
-        // Mensaje de exito y envio del token requerido
-        console.log("webhook verificado!");
-        res.status(200).send(req.query["hub.challenge"]);
-    } else {
-        // Mensaje de fallo
-        console.error("La verificacion ha fallado, porque los tokens no coinciden");
-        res.sendStatus(403);
-    }
-});
+app.get("/webhook", (req, res)=> chatbot.verification(req, res));
 
-// Todos eventos de mesenger sera apturados por esta ruta
-app.post("/webhook", function (req, res) {
-    // Verificar si el vento proviene del pagina asociada
-    if (req.body.object == "page") {
-        // Si existe multiples entradas entraas
-        req.body.entry.forEach(function(entry) {
-            // Iterara todos lo eventos capturados
-            entry.messaging.forEach(function(event) {
-                if (event.message) {
-                    process_event(event);
-                }
-            });
-        });
-        res.sendStatus(200);
-    }
-});
+// Todos eventos de mesenger seran capturados por esta ruta
+app.post("/webhook", (req, res)=> chatbot.responseHandler(req, res));
 
 
-// Funcion donde se procesara el evento
-function process_event(event){
-    // Capturamos los datos del que genera el evento y el mensaje 
-    var senderID = event.sender.id;
-    var message = event.message;
-    
-    // Si en el evento existe un mensaje de tipo texto
-    if(message.text){
-        // Crear un payload para un simple mensaje de texto
-        var response = {
-            "text": 'Enviaste este mensaje: ' + message.text
-        }
-    }
-    
-    // Enviamos el mensaje mediante SendAPI
-    enviar_texto(senderID, response);
-}
-
-// Funcion donde el chat respondera usando SendAPI
-function enviar_texto(senderID, response){
-    // Construcicon del cuerpo del mensaje
-    let request_body = {
-        "recipient": {
-          "id": senderID
-        },
-        "message": response
-    }
-    
-    // Enviar el requisito HTTP a la plataforma de messenger
-    request({
-        "uri": "https://graph.facebook.com/v2.6/me/messages",
-        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-        "method": "POST",
-        "json": request_body
-    }, (err, res, body) => {
-        if (!err) {
-          console.log('Mensaje enviado!')
-        } else {
-          console.error("No se puedo enviar el mensaje:" + err);
-        }
-    }); 
-}
